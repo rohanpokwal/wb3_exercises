d4924ddd4a6244d72df6f02e4abd47036c2275e4
Rohan Pokwal <rohan.pokwal@gmail.com>
2024-04-30 13:04:45 -0500

    completed simple_functions.js

---
+++ functions/simple_functions.js
@@ -0,0 +1,25 @@
+"use strict";
+
+//function to display my name, favorite movie, and favorite band
+function favoriteThings() {
+  console.log("My name is Eric");
+  console.log("My favorite movie is Almost Famous");
+  console.log("My favorite band is Metallica");
+}
+
+function whyImHere() {
+  console.log(
+    "I Joined the LearnToCode academy to get my foot in the software development field"
+  );
+}
+
+function favoritePlace() {
+  console.log(
+    "One Day I would Love to visit Japan because I heard that they are already living in future"
+  );
+}
+
+//Calling/Invokeing the function
+favoriteThings();
+whyImHere();
+favoritePlace();
